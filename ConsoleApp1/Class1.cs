8911243b464b194939ea7ea0292b00b5a5649ec1
luigicr95 <luigicr95@gmail.com>
2022-11-21 15:33:41 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Dipendente
+    {
+        private double _stipendio;
+
+        public double Stipendio { 
+            get { return _stipendio;}
+            set { _stipendio = value; }
+        }
+
+        public string BonusNatalizio()  //LA FUNZIONE HA UN VALORE DI RITORNO
+        {
+            return "Lo stipendio comprensivo di bonus è:" + (_stipendio + 100);
+        }
+
+        public void BonusNatalizioProcedura() //LA PROCEDURA RITORNA VOID
+        {
+            Console.WriteLine("Lo stipendio totale è:" + (_stipendio + 100));
+            //Console.WriteLine($"Lo stipendio totale è: {_stipendio+ 100}"); TEMPLATE LITERAL
+        }
+
+
+        public void AumentaStipendio(double aumento) {
+            _stipendio += aumento;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dipendente d1 = new Dipendente();
+            d1.Stipendio = 1250.00;
+            d1.AumentaStipendio(150);
+
+            Console.WriteLine(d1.Stipendio);
+
+            Console.WriteLine(d1.BonusNatalizio());
+            d1.BonusNatalizioProcedura();
+
+            Console.ReadLine();
+        }
+    }
+}
